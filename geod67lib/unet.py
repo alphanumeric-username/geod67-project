b5979b4ef92f80540a6cb8694d9ac29d4411e875
alphanumeric-username <geometrianaoeuclidiana@gmail.com>
2026-06-22 21:49:06 -0300

    fix: unet data format

--- geod67lib/unet.py
+++ geod67lib/unet.py
@@ -1,43 +1,46 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from tensorflow.keras import layers, Model
 
 
 
 def create_migration_map(nx, nz, name='migmap'):
-    input_layer = layers.Input(shape=(1, nx,nz))
+    input_layer = layers.Input(shape=(nx,nz, 1))
 
     encoder_channels = [ 8, 16, 32, 64, 128 ]
     decoder_channels = [ 64, 32, 16, 8 ]
 
     x = input_layer
     skips = []
-    print('encs')
+    # print('enc')
     for i, nc in enumerate(encoder_channels):
         x = _3x3Conv_BatchNorm_ReLU(nc, i!=0)(x)
         skips.insert(0, x)
-        print(x)
+        # print(x)
     skips.pop(0)
 
 
-    print('\ndec:')
+    # print('\ndec:')
     for i, nc in enumerate(decoder_channels):
         x = _2x2TransConv_Concat_3x3Conv_BatchNorm_ReLU(nc, skips[i])(x)
-        print(x)
+        # print(x)
     
-    output_layer = layers.Conv2D(1, (1,1), data_format='channels_first')(x)
+    # output_layer = layers.Conv2D(1, (1,1), data_format='channels_first')(x)
+    output_layer = layers.Conv2D(1, (1,1))(x)
 
     
-    return tf.keras.Model(inputs=input_layer, outputs=output_layer, name=name)
+    return Model(inputs=input_layer, outputs=output_layer, name=name)
 
 
 
 def _3x3Conv_BatchNorm_ReLU(nchannels, add_max_pooling=False):
     def f(x):
         if add_max_pooling:
-            x = layers.MaxPool2D((2,2), data_format='channels_first')(x)
+            # x = layers.MaxPool2D((2,2), data_format='channels_first')(x)
+            x = layers.MaxPool2D((2,2))(x)
 
-        x = layers.Conv2D(nchannels, (3,3), data_format='channels_first', padding='same')(x)
-        x = layers.BatchNormalization(axis=1)(x)
+        # x = layers.Conv2D(nchannels, (3,3), data_format='channels_first', padding='same')(x)
+        x = layers.Conv2D(nchannels, (3,3), padding='same')(x)
+        x = layers.BatchNormalization(axis=-1)(x)
         x = layers.ReLU()(x)
         return x
 
@@ -46,8 +49,9 @@ def _3x3Conv_BatchNorm_ReLU(nchannels, add_max_pooling=False):
 
 def _2x2TransConv_Concat_3x3Conv_BatchNorm_ReLU(nchannels, concat_input):
     def f(x):
-        x = layers.Conv2DTranspose(nchannels, (2,2), data_format='channels_first', strides=2)(x)
-        x = layers.Concatenate(axis=0)([x, concat_input[:,:, :x.shape[2], :x.shape[3]]])
+        # x = layers.Conv2DTranspose(nchannels, (2,2), data_format='channels_first', strides=2)(x)
+        x = layers.Conv2DTranspose(nchannels, (2,2), strides=2)(x)
+        x = layers.Concatenate(axis=-1)([x, concat_input[:,:, :x.shape[2], :x.shape[3]]])
         # x = layers.Concatenate(axis=0)([x, concat_input])
         x = _3x3Conv_BatchNorm_ReLU(nchannels)(x)
 
