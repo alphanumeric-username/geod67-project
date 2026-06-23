ea2943a582fb47e07f6bab5b56f2d52713bc2b0a
alphanumeric-username <geometrianaoeuclidiana@gmail.com>
2026-06-22 21:44:55 -0300

    add: finite differences module now supports arbitrary order operators

--- geod67lib/fd.py
+++ geod67lib/fd.py
@@ -1,67 +1,47 @@
-# Finite differences methods
+# Finite differences method
 
 import numpy as np
+import tensorflow as tf
 from scipy.special import factorial
 
 
+_cached_coeffs = {}
 def taylor_coeffs(k):
-    e = np.arange(2*k + 1)
-    d = factorial(e)
+    if not(k in _cached_coeffs):
+        e = np.arange(2*k + 1)
+        d = factorial(e)
 
-    X = np.array([
-        i**e/d for i in range(k, -k-1, -1)
-    ])
+        X = np.array([
+            i**e/d for i in range(k, -k-1, -1)
+        ], dtype=np.float32)
 
-    W = np.linalg.inv(X)
+        W = np.linalg.inv(X)
+        _cached_coeffs[k] = W
+        
+    return _cached_coeffs[k]
 
-    return W
 
+_cached_kernels = {}
+def d(f, h, axis=0, fd_order=2, op_order=1, **kwargs):
+    # cache_key = (fd_order, op_order, axis)
 
-
-def shift2d(f, s, axis=0):
-    g = np.roll(f, -s, axis)
-    if s == 0:
-        return f
+    # kernel = None
+    # if not(cache_key in _cached_kernels):
+    coeffs = taylor_coeffs(fd_order//2)[op_order]/h**op_order
+    coeffs = np.flip(coeffs)
+    n = coeffs.shape[0]
+    kernel = np.zeros((n,n,1,1), dtype=np.float32)
     if axis == 0:
-        if s > 0:
-            for i in range(1, int(np.abs(s)) + 1):
-                g[-i:, :] = f[-1, :]
-        else:
-            for i in range(0, int(np.abs(s))):
-                g[i, :] = f[0, :]
-    elif axis == 1:
-        if s > 0:
-            for i in range(1, int(np.abs(s)) + 1):
-                g[:, -i] = f[:, -1]
-        else:
-            for i in range(0, int(np.abs(s))):
-                g[:, i] = f[:, 0]
+        kernel[:, n//2, 0, 0] = coeffs
+    else:
+        kernel[n//2, :, 0, 0] = coeffs
+    kernel = tf.constant(kernel)
+    #     _cached_kernels[cache_key] = kernel.numpy()
+    # else:
+    #     kernel = tf.constant(_cached_kernels[cache_key])
 
-    return g
-
-
-def shift(f, s, axis=0, **kwargs):
-    n = np.abs(s, dtype=np.int32)
-    fpad = np.pad(f, n, **kwargs)
-    
-    return np.roll(fpad, -s, axis)[*([slice(n, -n)]*len(f.shape))]
-    
-
-
-def d2(f, h, axis=0, **kwargs):
-    f_p1 = shift(f, 1, axis, **kwargs)
-    f_0 = f
-    f_m1 = shift(f, -1, axis, **kwargs)
-
-    return 1/h**2 * (f_p1 - 2 * f_0 + f_m1)
-
-
-def d(f, h, axis=0, **kwargs):
-    f_p1 = shift(f, 1, axis, **kwargs)
-    f_0 = f
-    f_m1 = shift(f, -1, axis, **kwargs)
-
-    return 1/h**2 * (f_p1 - 2 * f_0 + f_m1)
+    out = tf.nn.conv2d(f, kernel, strides=1, padding='SAME')
+    return out
 
 
 
@@ -69,8 +49,45 @@ def lap(f, hx, hz, **kwargs):
     """
     Second-order 2D laplacian operator
     """
-    return d2(f, hx, 0, **kwargs) + d2(f, hz, 1, **kwargs)
+    return d(f, hx, axis=0, op_order=2, **kwargs) + d(f, hz, axis=1, op_order=2, **kwargs)
 
 
 def grad(f, hx, hz, **kwargs):
-    d2(f, hx, 0, **kwargs), d2(f, hz, 1, **kwargs)
+    d(f, hx, axis=0, **kwargs), d(f, hz, axis=1, **kwargs)
+
+
+
+class DifferentialOperator():
+    def __init__(self, fd_order, hx, hz):
+        self.W = taylor_coeffs(fd_order//2)
+        self.kernels = {}
+        h = [hx, hz]
+        for axis in [0, 1]:
+            for op_order in range(self.W.shape[0]):
+                key = (axis, op_order)
+
+                coeffs = self.W[op_order]/h[axis]**op_order
+                coeffs = np.flip(coeffs)
+                n = coeffs.shape[0]
+                kernel = np.zeros((n,n,1,1), dtype=np.float32)
+                if axis == 0:
+                    kernel[:, n//2, 0, 0] = coeffs
+                else:
+                    kernel[n//2, :, 0, 0] = coeffs
+                kernel = tf.constant(kernel)
+                
+                self.kernels[key] = kernel
+    
+
+    def d(self, f, axis=0, op_order=1):
+        kernel = self.kernels[(axis, op_order)]
+        out = tf.nn.conv2d(f, kernel, strides=1, padding='SAME')
+        return out
+    
+
+    def lap(self, f):
+        return self.d(f, axis=0, op_order=2) + self.d(f, axis=1, op_order=2)
+
+
+    def grad(self, f):
+        self.d(f, axis=0, op_order=1), self.d(f, axis=1, op_order=1)
